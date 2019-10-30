eda0727d8a820cf3f4d7c925c0c4ef8c32d4d661
League Student <league@iMac-15.attlocal.net>
2019-10-29 18:28:36 -0700

    finished array sorter, took quiz and worked on arraylists

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,14 +1,24 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> names = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		names.add("Jame");
+		names.add("Sara");
+		names.add("Fred");
+		names.add("Marc");
+		names.add("Dane");
 		//3. Print all the Strings using a standard for-loop
-		
+		System.out.println("Here's an ArrayList of Strings:\n");
+	
+		for(int i = 0; i < names.size(); i++){
+			String s = names.get(i);
+			System.out.println("String at element " + i + " " + s);
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
@@ -17,4 +27,5 @@ public class IntroToArrayLists {
 		
 		//7. Print only the Strings that have the letter 'e' in them.
 	}
+}
 }
